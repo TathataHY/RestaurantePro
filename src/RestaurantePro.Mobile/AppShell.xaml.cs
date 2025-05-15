c13cda68cceea0794ed3d1c25878839898c1503a
Tathata <andre.yovera1@gmail.com>
2025-05-15 14:02:58 -0400

    Eliminación de archivos obsoletos en el proyecto RestaurantePro
    - Se han eliminado múltiples archivos de código y configuración en las carpetas `Infrastructure`, `Mobile`, y `Web`, incluyendo clases, scripts, y recursos que ya no son necesarios.
    - Esta limpieza contribuye a un proyecto más organizado y enfocado, eliminando código redundante y facilitando el mantenimiento futuro.

--- src/RestaurantePro.Mobile/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantePro.Mobile;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- src/RestaurantePro.Mobile/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace RestaurantePro.Mobile;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
